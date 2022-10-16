0ece2d64075355225f7c39180b30495265a61440
unknown <sumaiyahas2908@gmail.com>
2022-10-16 19:49:34 +0530

    refactor: add index to components

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Loader } from './Loader/Loader'
+export { default as Navbar } from './Navbar/Navbar'
+export { default as ResourceContainer } from './Resources/ResoureContainer'
+export { default as ItemTable } from './ItemTable/ItemTable'

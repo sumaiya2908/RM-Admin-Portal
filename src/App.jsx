fd1832b3f207dab8c195883bb5a1091b02f57336
unknown <sumaiyahas2908@gmail.com>
2022-10-16 19:48:52 +0530

    feat: add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import './App.scss'
+import { Navbar } from './components'
+import { Home, Resource } from './pages'
 
 function App() {
 
 	return (
-		<div className="App">
-			RM Admin Portal
-		</div>
+		<BrowserRouter>
+			<Navbar />
+			<div className='app'>
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route path="/resource/:id" element={<Resource />} />
+				</Routes>
+			</div>
+		</BrowserRouter>
+
 	)
 }
 

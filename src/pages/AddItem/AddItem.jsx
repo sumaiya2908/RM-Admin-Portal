a8619622a6d326648a99f28bff4faac66cee02db
unknown <sumaiyahas2908@gmail.com>
2022-10-26 14:17:55 +0530

    feat: add additem form

--- src/pages/AddItem/AddItem.jsx
+++ src/pages/AddItem/AddItem.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
-
-const AddItem = () => {
+import React from "react";
+import { useLocation, useParams } from "react-router-dom";
+import Image from "../../assets/form-image.png";
+import "./AddItem.scss";
+const AddItem = (props) => {
+	console.log(props)
+	// const { setItems } = useLocation().state
+	console.log(useLocation())
 	return (
-		<div>AddItem</div>
-	)
-}
+		<div className='add-item'>
+			<div className='form'>
+				<form className=''>
+					<div className='heading'>Item Details</div>
 
-export default AddItem
+					<label>ITEM NAME</label>
+					<input
+						type='text'
+						className='field'
+						placeholder='Enter Item Name'
+					/>
+					<label>LINK</label>
+					<input
+						type='text'
+						className='field'
+						placeholder='Enter Link'
+					/>
+					<label>RESOURCE NAME</label>
+					<input
+						type='text'
+						className='field'
+						placeholder='Enter Resource Name'
+					/>
+					<label>DESCRIPTION</label>
+					<input
+						type='text'
+						className='field'
+						placeholder='Enter Description'
+					/>
+					<button className='button-primary'>Create</button>
+				</form>
+			</div>
+			<div className='image'>
+				<img src={Image} />
+			</div>
+		</div>
+	);
+};
+
+export default AddItem;

9954a118bd7e7a273ca3bc13dffe8a10e4566b2a
unknown <sumaiyahas2908@gmail.com>
2022-10-30 22:48:45 +0530

    feat: add resource component

--- src/pages/Resource/Resource.jsx
+++ src/pages/Resource/Resource.jsx
@@ -1,18 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 
-import { ItemTable } from '../../components'
-import './Resource.scss';
-import getRequest from '../../utils/axiosRequest'
+import { ItemTable } from "../../components";
+import getRequest from "../../utils/axiosRequest";
+import "./Resource.scss";
 
 const Resource = () => {
-	const [resource, setResource] = useState([])
+	const [resource, setResource] = useState([]);
+	const [status, setStatus] = useState();
+	const [error, setError] = useState(null)
+
 	const { id } = useParams();
-	const url = `https://media-content.ccbp.in/website/react-assignment/resource/${id}.json`
+	const url = `https://media-content.ccbp.in/website/react-assignment/resource/${id}.json`;
 
 	useEffect(() => {
-		getRequest(url, setResource)
-	}, [])
+		getRequest(url, setResource);
+	}, []);
+
+	useEffect(() => {
+		if (status == 200) {
+			toast.success('Resource Updated Successfully!', {
+				position: toast.POSITION.TOP_RIGHT,
+				autoClose: 5000
+			})
+			setStatus('')
+		}
+		if (error) {
+			toast.error(error, {
+				position: toast.POSITION.TOP_RIGHT,
+				autoClose: 5000
+			})
+			setError(null)
+		}
+
+	}, [status, error])
+
+	const handleUpdate = () => {
+		const url = 'https://media-content.ccbp.in/website/react-assignment/resource/update.json'
+		const r = axios.get(url)
+			.then((res) => setStatus(res.status))
+			.catch((err) => setError(err))
+	}
 
 	return (
 		<>
@@ -22,25 +52,23 @@ const Resource = () => {
 						<img src={resource.icon_url} />
 					</div>
 					<div className='text'>
-						<div className='title'>
-							{resource.title}
-						</div>
-						<a className='link' href={resource.link} target='_blank'>
+						<div className='title'>{resource.title}</div>
+						<a
+							className='link'
+							href={resource.link}
+							target='_blank'
+						>
 							{resource.link}
 						</a>
 					</div>
 				</div>
-				<div className='description'>
-					{resource.description}
-				</div>
+				<div className='description'>{resource.description}</div>
 			</div>
-			<button className='primary-button'>
-				Update
-			</button>
+			<button className='primary-button' onClick={handleUpdate}>Update</button>
 
-			<ItemTable items={resource.resource_items} />
+			{resource.resource_items && <ItemTable items={resource.resource_items} />}
 		</>
-	)
-}
+	);
+};
 
-export default Resource
+export default Resource;

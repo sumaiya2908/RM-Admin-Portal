c53c0282ae29f8d59543fbf400fb2e7f26fc4841
unknown <sumaiyahas2908@gmail.com>
2022-10-30 22:47:57 +0530

    feat: add additem form

--- src/pages/AddItem/AddItem.jsx
+++ src/pages/AddItem/AddItem.jsx
@@ -1,48 +1,86 @@
-import React from "react";
-import { useLocation, useParams } from "react-router-dom";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
 import Image from "../../assets/form-image.png";
 import "./AddItem.scss";
-const AddItem = (props) => {
-	console.log(props)
-	// const { setItems } = useLocation().state
-	console.log(useLocation())
-	return (
-		<div className='add-item'>
-			<div className='form'>
-				<form className=''>
-					<div className='heading'>Item Details</div>
 
-					<label>ITEM NAME</label>
-					<input
-						type='text'
-						className='field'
-						placeholder='Enter Item Name'
-					/>
-					<label>LINK</label>
-					<input
-						type='text'
-						className='field'
-						placeholder='Enter Link'
-					/>
-					<label>RESOURCE NAME</label>
-					<input
-						type='text'
-						className='field'
-						placeholder='Enter Resource Name'
-					/>
-					<label>DESCRIPTION</label>
-					<input
-						type='text'
-						className='field'
-						placeholder='Enter Description'
-					/>
-					<button className='button-primary'>Create</button>
-				</form>
+const AddItem = () => {
+	const [status, setStatus] = useState();
+	const [error, setError] = useState(null)
+	const navigate = useNavigate();
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		let url =
+			"https://media-content.ccbp.in/website/react-assignment/add_resource.json";
+		let r = axios.get(url)
+			.then((result) => setStatus(result.status))
+			.catch((err) => setError(err))
+
+	};
+
+	useEffect(() => {
+		if (status == 200) {
+			toast.success('Item added Successfully!', {
+				position: toast.POSITION.TOP_RIGHT,
+				autoClose: 5000,
+			});
+			navigate("/")
+		}
+		if (error) {
+			toast.error(error.msg, {
+				position: toast.POSITION.TOP_RIGHT,
+				autoClose: 5000
+			})
+		}
+	}, [status, error])
+
+	return (
+		<>
+			<div className='add-item'>
+				<div className='form'>
+					<form className='' onSubmit={handleSubmit}>
+						<div className='heading'>Item Details</div>
+
+						<label>ITEM NAME</label>
+						<input
+							type='text'
+							className='field'
+							placeholder='Enter Item Name'
+							name='title'
+						/>
+						<label>LINK</label>
+						<input
+							type='text'
+							className='field'
+							placeholder='Enter Link'
+							name='link'
+						/>
+						<label>RESOURCE NAME</label>
+						<input
+							type='text'
+							className='field'
+							placeholder='Enter Resource Name'
+							name='resource_name'
+						/>
+						<label>DESCRIPTION</label>
+						<input
+							type='text'
+							classame='field'
+							placeholder='Enter Description'
+						/>
+						<button type='submit' className='button-primary'>
+							Create
+						</button>
+					</form>
+				</div>
+				<div className='image'>
+					<img src={Image} />
+				</div>
+
 			</div>
-			<div className='image'>
-				<img src={Image} />
-			</div>
-		</div>
+		</>
 	);
 };
 
